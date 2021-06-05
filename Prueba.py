ca7952baf006e3598d2e1b43558958c514f73568
Rodrigo Tejada <67207372+Tejada-coder@users.noreply.github.com>
2021-06-05 10:09:14 -0500

    I added to a new comment

--- Prueba.py
+++ Prueba.py
@@ -1,2 +1,4 @@
-#Este va a ser un archivo python que servira de ejemplo para el curso de git y github
+#This is a python file, help like example for git and github
 print('Este sera un ejemplo para el uso de git y github')
+
+#We try help the people with our code
